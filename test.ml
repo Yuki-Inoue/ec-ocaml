8b14edb590752814a2d83121bc467c667ded6898
Yuki Inoue <inouetakahiroki@gmail.com>
2013-01-21 16:04:55 +0900

    test_play in test.ml

--- test.ml
+++ test.ml
@@ -10,3 +10,12 @@ let rec dumb_play node =
   if terminal node then node
   else
     dumb_play (play node (List.hd (possible_moves node)))
+
+
+let test_node = ref initial
+let test_moves = ref (possible_moves initial)
+
+let test_play i =
+  test_node := play !test_node (List.nth !test_moves i);
+  test_moves := possible_moves !test_node;
+  !test_node
